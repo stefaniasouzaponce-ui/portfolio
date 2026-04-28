9c347aa285767921a16fe5f7fda0c053546524b8
Stefania <stefania.souzaponce@gmail.com>
2026-04-27 22:20:12 -0300

    Fix handler export in chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,3 +1,5 @@
+// v2
+module.exports = async function handler(req, res) {
 module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
